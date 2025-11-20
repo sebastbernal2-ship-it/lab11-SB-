05363ca0f8ce00a3481f4d797f187fcbeafd54d4
Sebastian Bernal <sebastbernal2@gmail.com>
2025-11-19 21:36:30 -0500

    modified calculator

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,5 @@
 import unittest
+import calculator
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
@@ -50,6 +51,35 @@ class TestCalculator(unittest.TestCase):
     #     # Test basic function
     #     fill in code
     ##########################
+    def test_multiply(self):
+        self.assertEqual(calculator.multiply(2, 3), 6)
+        self.assertEqual(calculator.multiply(-4, 2), -8)
+        self.assertEqual(calculator.multiply(0, 5), 0)
+
+    def test_divide(self):
+        self.assertEqual(calculator.divide(10, 2), 5)
+        self.assertEqual(calculator.divide(-10, 2), -5)
+        self.assertAlmostEqual(calculator.divide(5, 2), 2.5)
+
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            calculator.logarithm(2, -8)
+        with self.assertRaises(ValueError):
+            calculator.logarithm(2, 0)
+
+    def test_hypotenuse(self):
+        # Hypotenuse of sides 3, 4 is 5
+        self.assertEqual(calculator.hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(calculator.hypotenuse(6, 8), 10)
+
+    def test_sqrt(self):
+        self.assertEqual(calculator.square_root(9), 3)
+        self.assertAlmostEqual(calculator.square_root(2), 2 ** 0.5)
+        with self.assertRaises(ValueError):
+            calculator.square_root(-4)
+
+
+
 
 # Do not touch this
 if __name__ == "__main__":
